955d2b31d56057a057e2df0dbd63eef3b86051d5
NMaster23 <nmmaster22@gmail.com>
2026-05-03 15:27:55 -0700

    Implemented UI

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
+use mdns::resolve;
 use mdns_sd::{ServiceDaemon, ServiceInfo, ServiceEvent};
 use async_std::stream::StreamExt;
 use mdns_sd::Error;
 use bluest::*;
 use local_ip_address::local_ip;
+use slint::{SharedString, VecModel};
 use std::collections::HashMap;
-use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::fs::File;
 use std::io;
+use slint::Model;
+use std::rc::Rc;
 
 slint::include_modules!();
-
-struct DeviceInfo {
-    addresses: Vec<String>,
-    port: u16,
-}
 
 struct BlueData {
     identifier: String,
@@ -73,14 +71,12 @@ async fn bluetooth() {
         }
 }
 
-async fn wifi(mdns: ServiceDaemon) {
-    let mut devices: HashMap<String, DeviceInfo> = HashMap::new();
+async fn wifi(mdns: ServiceDaemon, ui_handle: slint::Weak<AppWindow>) {
     let service_type = "_rustdrop._tcp.local.";
     let instance_name = "rustdrop";
     let host_name = "rustdrop.local.";
     let port = 5200;
     let receiver = mdns.browse(service_type).expect("Failed to browse");
-    let receiver_info = receiver.clone();
     let ip = local_ip().unwrap().to_string();
     let rustdrop_service = ServiceInfo::new(
         service_type,
@@ -91,46 +87,48 @@ async fn wifi(mdns: ServiceDaemon) {
         None,
     ).unwrap();
     mdns.register(rustdrop_service).expect("Failed to register our service");
-    std::thread::spawn(|| {
+    async_std::task::spawn(async move {
         receive_file_wifi();
     });
-    std::thread::spawn(move || {
-        while let Ok(event) = receiver_info.recv() {
-            match event {
-                ServiceEvent::ServiceResolved(resolved) => {
-                    let device_name = resolved.get_fullname().to_string();
-                    let info = DeviceInfo {
-                        addresses: resolved.get_addresses().iter().map(|ip| ip.to_string()).collect(),
-                        port: resolved.get_port(),
-                    };
-                    devices.insert(device_name, info);
-                }
-                _ => {}
+    async_std::task::spawn(async move {
+        while let Ok(event) = receiver.recv() {
+            if let ServiceEvent::ServiceResolved(resolved) = event {
+                println!("Resolved a new service: {}", resolved.fullname);
+                let name = resolved.get_hostname().to_string();
+                let ip = resolved.get_addresses().into_iter().map(|a| a.to_string()).next().unwrap_or_default();
+                ui_handle.upgrade_in_event_loop(move |ui| {
+                    let mut devices: Vec<WifiDevice> = ui.get_wifi_devices().iter().collect();
+                    devices.push(WifiDevice {
+                        name: name.into(),
+                        ip: ip.into()
+                    });
+                    ui.set_wifi_devices(slint::ModelRc::from(Rc::new(slint::VecModel::from(devices))));
+                }).unwrap();
             }
-        }
-    });
-
-    println!("Enter file path to send:");
-    let mut file_path = String::new();
-    io::stdin().read_line(&mut file_path).expect("failed to readline");
-    println!("Enter target IP address:");
-    let mut ip_addr = String::new();
-    io::stdin().read_line(&mut ip_addr).expect("failed to readline");
-    
-    send_file_wifi(ip_addr.trim().to_string(), 5200, file_path.trim());
+            }
+        });
+//    println!("Enter file path to send:");
+//    let mut file_path = String::new();
+//   io::stdin().read_line(&mut file_path).expect("failed to readline");
+ //  println!("Enter target IP address:");
+//    let mut ip_addr = String::new();
+//    io::stdin().read_line(&mut ip_addr).expect("failed to readline");
+//    send_file_wifi(ip_addr.trim().to_string(), 5200, file_path.trim());
 }
 
 #[async_std::main]
 async fn main() -> Result<(), Error> {
     let ui = AppWindow::new().unwrap();
     let mdns = ServiceDaemon::new().expect("Failed to create daemon");
+    let ui_clone = ui.as_weak();
     ui.on_send_mode(move |blue_or_wifi: bool| {
         if blue_or_wifi {
             async_std::task::spawn(bluetooth());
         }
         if !blue_or_wifi {
             let mdns_clone = mdns.clone();
-            async_std::task::spawn(wifi(mdns_clone));
+            let wifi_ui = ui_clone.clone();
+            async_std::task::spawn(wifi(mdns_clone, wifi_ui));
         }
     });
     ui.run().expect("UI Initialization Error");
