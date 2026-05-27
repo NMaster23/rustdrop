dfafcee6498d20737573ece8a5af7021b114af20
NMaster23 <nmmaster22@gmail.com>
2026-05-27 16:31:22 -0700

    Implemented Working FIle Picker
    I used some of Android Studio's built-in autocomplete on this. I will probably continue using that. I also used some AI generated examples to base my code off of.

--- RustDropAndroid2/app/src/main/java/com/nmaster23/rustdrop/android/MainActivity.kt
+++ RustDropAndroid2/app/src/main/java/com/nmaster23/rustdrop/android/MainActivity.kt
@@ -1,19 +1,37 @@
 package com.nmaster23.rustdrop.android
 
+import android.R
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.nmaster23.rustdrop.android.ui.theme.RustDropAndroidTheme
+import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Column
+
+private var selectedUri by mutableStateOf<Uri?>(null)
 
 class MainActivity : ComponentActivity() {
+    private val openDocumentLauncher = registerForActivityResult(
+        ActivityResultContracts.OpenDocument()
+    ) { uri: Uri? ->
+        selectedUri = uri
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,27 +39,34 @@ class MainActivity : ComponentActivity() {
             RustDropAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
+                        onOpenFile = { openFile() }
                     )
                 }
             }
         }
     }
+    fun openFile() {
+        openDocumentLauncher.launch(arrayOf("application/pdf"))
+    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier, onOpenFile: () -> Unit) {
+    Column(modifier = modifier) {
+        Text(
+            text = selectedUri?.toString() ?: "No file selected"
+        )
+        Button(onClick = onOpenFile) {
+            Text("Select File")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     RustDropAndroidTheme {
-        Greeting("Android")
+        Greeting(onOpenFile = {})
     }
 }
